d002865322537751d6f19ad964f768b02e74a077
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2025-04-23 00:22:48 +0200

    visualize

---
+++ E_Visualize/main_visuals.py
@@ -0,0 +1,22 @@
+# resources:
+# https://www.ida.liu.se/~robke04/SHACLTutorial/Introduction%20to%20SHACL.pdf
+# https://shacl.org/playground/
+# http://www.validatingrdf.com/
+# https://15926.org/topics/SHACL/index.htm
+
+
+from pathlib import Path
+
+from otlmow_visuals.PyVisWrapper import PyVisWrapper
+
+
+from C_LDES.objects import create_objects
+
+if __name__ == '__main__':
+    objects = create_objects()
+    model_dir = Path(__file__).parent.parent / 'VLAG_model'
+    # requires modification of the PyVisWrapper to use the model directory
+    # and color the relations
+    wrapper = PyVisWrapper( model_directory=model_dir)
+    wrapper.show(list_of_objects=objects, html_path=Path('show.html', model_directory=model_dir))
+
