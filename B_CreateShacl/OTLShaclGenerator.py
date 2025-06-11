65889a5afbbd17fe42c38aa0d1ef66a05bf1bfd4
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2025-06-11 13:54:45 +0200

    {Euro} to € in shacl

--- B_CreateShacl/OTLShaclGenerator.py
+++ B_CreateShacl/OTLShaclGenerator.py
@@ -213,7 +213,7 @@ class OTLShaclGenerator:
                 if row[10] == str(RDFS.Literal):
                     if '"^^cdt:ucumunit' in row[12]:
                         unit = row[12].split('"')[1]
-                        if unit == '{Euro}':
+                        if unit == '{Euro}' or unit == 'Euro':
                             unit = '€'
                         g.add((attribute_node_ref, SH.pattern, Literal(unit)))
 
